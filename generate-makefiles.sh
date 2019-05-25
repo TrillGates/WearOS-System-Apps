6c218b8b88abd3514260ea9d70fa1d6cae719df6
davwheat <blokmaster.help@gmail.com>
2019-05-25 11:22:38 +0100

    fix(makefiles): add privilages for system apps

--- generate-makefiles.sh
+++ generate-makefiles.sh
@@ -1,4 +1,4 @@
-rm -r **/Android.mk
+rm -rf **/Android.mk
 
 for d in */; do
     cd $d
@@ -7,6 +7,7 @@ for d in */; do
     echo "LOCAL_MODULE_TAGS := optional" >> Android.mk
     echo "LOCAL_MODULE := ${d::-1}" >> Android.mk
     echo "LOCAL_CERTIFICATE := PRESIGNED" >> Android.mk
+    echo "LOCAL_PRIVILEGED_MODULE := true" >> Android.mk # For system apps
     echo "LOCAL_SRC_FILES := ${d::-1}.apk" >> Android.mk
     echo "LOCAL_MODULE_CLASS := APPS" >> Android.mk
     echo "LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)" >> Android.mk
